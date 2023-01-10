babf0dc40014b7e2c35f97d5eded6dcba7f11ad8
davidcoats <david@getsafetycone.com>
2023-01-10 09:56:52 +0530

    Initial commit.

---
+++ source/R5T.F0099/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0099
+{
+	/// <summary>
+	/// Assembly documentation file related functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0099/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0099
+Assembly documentation file related functionality.
